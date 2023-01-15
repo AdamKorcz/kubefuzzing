af57f4dfa510665122fb15daba452357836ad560
AdamKorcz <adam@adalogics.com>
2023-01-15 18:57:14 +0000

    add more funcs
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1
 	github.com/golang/protobuf v1.5.2
 	k8s.io/apimachinery v0.26.0
+	knative.dev/pkg v0.0.0-20230113013451-8abadb0a3c19
 )
 
 require (
@@ -14,7 +15,7 @@ require (
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
-	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
@@ -23,8 +24,9 @@ require (
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	k8s.io/klog/v2 v2.80.1 // indirect
-	k8s.io/utils v0.0.0-20221107191617-1a15be271d1d // indirect
+	k8s.io/api v0.26.0 // indirect
+	k8s.io/klog/v2 v2.80.2-0.20221028030830-9ae4992afb54 // indirect
+	k8s.io/utils v0.0.0-20221108210102-8e77b1f39fe2 // indirect
 	sigs.k8s.io/json v0.0.0-20220713155537-f223a00ba0e2 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
---
+++ pkg/roundtrip/knative_fuzzer_funcs.go
@@ -0,0 +1,105 @@
+// Copyright 2023 the kubefuzzing authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package roundtrip
+
+import (
+	//"math/rand"
+	"net/url"
+
+	fuzz "github.com/AdaLogics/go-fuzz-headers"
+	//corev1 "k8s.io/api/core/v1"
+	//"k8s.io/apimachinery/pkg/api/apitesting/fuzzer"
+	//"k8s.io/apimachinery/pkg/runtime/serializer"
+	"knative.dev/pkg/apis"
+)
+
+var (
+	Funcs = FuzzerFuncs()
+)
+
+// Funcs includes fuzzing funcs for knative.dev/serving types
+//
+// For other examples see
+// https://github.com/kubernetes/apimachinery/blob/master/pkg/apis/meta/fuzzer/fuzzer.go
+func FuzzerFuncs() []interface{} {
+	return []interface{}{
+		func(u *apis.URL, c fuzz.Continue) error {
+			schemeString, err := c.F.GetStringFrom("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", 50)
+			if err != nil {
+				return err
+			}
+			hostString, err := c.F.GetStringFrom("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", 50)
+			if err != nil {
+				return err
+			}
+			user, err := c.F.GetStringFrom("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", 50)
+			if err != nil {
+				return err
+			}
+			p, err := c.F.GetStringFrom("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", 50)
+			if err != nil {
+				return err
+			}
+			u.Scheme = schemeString
+			u.Host = hostString
+			u.User = url.UserPassword(
+				user, // username
+				p, // password
+			)
+			rawPath, err := c.F.GetString()
+			if err != nil {
+				return err
+			}
+			rawQuery, err := c.F.GetString()
+			if err != nil {
+				return err
+			}
+			u.RawPath = url.PathEscape(rawPath)
+			u.RawQuery = url.QueryEscape(rawQuery)
+		return nil
+		},
+	}
+}
+
+// FuzzConditions fuzzes the values for the conditions. It doesn't add
+// any new condition types
+//
+// Consumers should initialize their conditions prior to fuzzing them.
+// For example:
+//
+//	func(s *SomeStatus, c fuzz.Continue) {
+//	  c.FuzzNoCustom(s) // fuzz the status object
+//
+//	  // Clear the random fuzzed condition
+//	  s.Status.SetConditions(nil)
+//
+//	  // Fuzz the known conditions except their type value
+//	  s.InitializeConditions()
+//	  fuzz.Conditions(&s.Status, c)
+//	}
+/*func FuzzConditions(accessor apis.ConditionsAccessor, c fuzz.Continue) {
+	conds := accessor.GetConditions()
+	for i, cond := range conds {
+		// Leave condition.Type untouched
+		cond.Status = corev1.ConditionStatus(c.RandString())
+		cond.Severity = apis.ConditionSeverity(c.RandString())
+		cond.Message = c.RandString()
+		cond.Reason = c.RandString()
+		c.FuzzNoCustom(&cond.LastTransitionTime)
+		conds[i] = cond
+	}
+	accessor.SetConditions(conds)
+}*/
